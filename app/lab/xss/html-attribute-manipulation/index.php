bb64322973ab954b6720885205082a7c25adf5ce
angel f <angelf@outlook.com>
2024-02-26 20:50:51 +0100

    a

--- app/lab/xss/html-attribute-manipulation/index.php
+++ app/lab/xss/html-attribute-manipulation/index.php
@@ -12,6 +12,7 @@ function encodeB($char){
 <html lang="en">
 
 <head>
+    
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
